5bfb17d822a87b7d87776f710f699c19b0b8f21c
godlike-paul <1741651197@qq.com>
2023-04-20 16:05:33 +0800

    骨架

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,21 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+
+    @RequestMapping("getBotInfo/")
+    public Map<String,String> getBotInfo(){
+        Map<String,String> bot=new HashMap<>();
+        bot.put("name","gg");
+        bot.put("rating","1500");
+        return bot;
+    }
+
+}
